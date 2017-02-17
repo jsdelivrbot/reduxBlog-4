d09f4985c4f433b6cfdad5b9cb952d129422630f
Nimrod Bahar <nimrodbahar@gmail.com>
2017-02-17 14:34:01 +0100

    A react.js simple blog

---
+++ src/components/posts_index.js
@@ -0,0 +1,24 @@
+import React, { Component} from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
+
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPosts }, dispatch);
+};
+
+export default connect (null, mapDispatchToProps)(PostsIndex);
+
+// A shorthand to remove the mapDispatchToProps method
+// export default connect (null, { fetchPosts })(PostsIndex);
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, IndexRoute} from 'react-router';
+
+import App from './components/app.js';
+import PostIndex from './components/posts_index';
+
+
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={PostIndex} />
+  </Route>
+);
